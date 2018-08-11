ab0f8b8220a9fee627ce96ef2a2910196ecc7e5c
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-08-11 20:46:46 +0200

    initial
    added create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//style
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This will be a map app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
